faf01a4930e8d147813a30a8a339f9b6e085f446
ValeriaeWasTaken <valeries.dev@gmail.com>
2026-01-13 14:23:20 -0700

    7.4 part1

--- Material UI - Reborn/pack.bat
+++ Material UI - Reborn/pack.bat
@@ -1,3 +1,3 @@
-del "C:\Users\Owner\Documents\GitHub\Material-UI-Reborn\Material UI - Reborn\packs\7.3.0.11.aeth"
+del "C:\Users\Owner\Documents\GitHub\Material-UI-Reborn\Material UI - Reborn\packs\7.4.0.12.aeth"
 "C:\Users\Owner\Documents\MUIR - Dev\a.exe" pack "C:\Users\Owner\Documents\GitHub\Material-UI-Reborn\Material UI - Reborn"
 @pause
